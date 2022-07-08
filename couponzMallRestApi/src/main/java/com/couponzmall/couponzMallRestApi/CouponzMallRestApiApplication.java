2914fd223b8ecaf374c86cd98ea486900dd1d972
sheetalj2205 <sheetalj2205@gmail.com>
2022-07-08 19:14:54 +0530

    Update: Dependency added for bootstrap

---
+++ couponzMallRestApi/src/main/java/com/couponzmall/couponzMallRestApi/CouponzMallRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.couponzmall.couponzMallRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponzMallRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponzMallRestApiApplication.class, args);
+	}
+
+}
---
+++ couponzMallRestApi/src/test/java/com/couponzmall/couponzMallRestApi/CouponzMallRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponzmall.couponzMallRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponzMallRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
